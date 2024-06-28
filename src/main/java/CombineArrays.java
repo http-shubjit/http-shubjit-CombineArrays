45532faed3ac86b9388f1689b1c18f9190ff2f5a
Shubhajit <shubjitgithub@gmail.com>
2024-06-28 06:16:32 +0530

    Done

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -7,8 +7,13 @@ public class CombineArrays {
      * @param arr2 an array of ints.
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
-    public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+    public int[] combine(int[] arr1, int[] arr2) {
+        int a1len = arr1.length;
+        int a2len = arr2.length;
+        int len = a1len+a2len;
+        int arr[] = new int[len];
+        System.arraycopy(arr1, 0, arr, 0, a1len);
+        System.arraycopy(arr2, 0, arr, a1len, a2len);
+        return arr;
     }
 }
